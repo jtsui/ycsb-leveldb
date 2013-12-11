513916818ab88d3585ccef8941af588e68e38b12
Jeff Tsui <tsui.jeff@berkeley.edu>
2013-12-11 00:51:44 -0800

    update graph

--- graph.py
+++ graph.py
@@ -1,12 +1,9 @@
 import sys
-from collections import defaultdict
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 FIELDS = ['[UPDATE]', '[INSERT]', '[READ]']
-LINE_TYPES = {'[READ]': '',
-              '[UPDATE]': '',
-              '[INSERT]': '',
-              }
+plt.ioff()
 
 
 def str_to_num(s):
@@ -16,115 +13,76 @@ def str_to_num(s):
         return False
 
 
-def throughput(log_files):
-    data = []
-    text = {}
-    with open(log_file) as f:
-        for line in f:
-            if line.startswith('Command line:'):
-                line_split = line.split(' ')
-                workload = 'unkown'
-                for i in range(len(line_split)):
-                    if line_split[i] == '-P':
-                        workload = line_split[i + 1]
-                title = 'Throughput for %s from %s' % (
-                    workload, log_file)
-            split_line = line.strip().split(' ')
-            if len(split_line) > 5 and split_line[1] == 'sec:' and split_line[3] == 'operations;':
-                data.append((split_line[0], split_line[4]))
-            vals = [x.strip() for x in line.split(',')]
-            if len(vals) == 3 and vals[0] == '[OVERALL]':
-                if vals[1] == 'RunTime(ms)':
-                    text[vals[1]] = str_to_num(vals[2])
-                elif vals[1] == 'Throughput(ops/sec)':
-                    text[vals[1]] = str_to_num(vals[2])
-    fig, ax = plt.subplots()
-    # line
-    x = [time for time, tp in data]
-    y = [tp for time, tp in data]
-    ax.plot(x, y)
-    plt.title(title)
-    plt.xlabel('time (sec)')
-    plt.ylabel('throughput (ops/sec)')
-    # table
-    row_labels = ['Overall']
-    col_labels = text.keys()
-    table_vals = []
-    row = []
-    for col in col_labels:
-        row.append(text[col])
-    table_vals.append(row)
-    table = ax.table(cellText=table_vals,
-                     rowLabels=row_labels,
-                     colLabels=col_labels,
-                     loc='lower right')
-    table.set_fontsize(8)
-    table.scale(0.85, 1.1)
-    plt.show()
+def throughput(log_files, save_or_show):
+    for log_file in log_files:
+        data = []
+        with open(log_file) as f:
+            for line in f:
+                split_line = line.strip().split(' ')
+                if len(split_line) > 5 and split_line[1] == 'sec:' and split_line[3] == 'operations;':
+                    data.append((split_line[0], split_line[4]))
+        x = [time for time, tp in data]
+        y = [tp for time, tp in data]
+        plt.plot(x, y, label=log_file.replace('.log', ''))
+    plt.title('Throughput')
+    plt.xlabel('time (s)')
+    plt.ylabel('ops/sec')
+    legend = plt.legend()
+    for label in legend.get_texts():
+        label.set_fontsize('small')
+    if save_or_show == 'show':
+        plt.show()
+    else:
+        plt.savefig('throughput.pdf', bbox_inches=0)
+        print 'Throughput graph saved to throughput.pdf'
 
 
-def latency(log_files):
-    data = defaultdict(list)
-    text = defaultdict(dict)
-    title = ''
-    with open(log_file) as f:
-        for line in f:
-            if line.startswith('Command line:'):
-                line_split = line.split(' ')
-                workload = 'unkown'
-                for i in range(len(line_split)):
-                    if line_split[i] == '-P':
-                        workload = line_split[i + 1]
-                title = 'Latency for %s from %s' % (
-                    workload, log_file)
-            vals = [x.strip() for x in line.split(',')]
-            if len(vals) == 3 and vals[0] in FIELDS:
-                time = str_to_num(vals[1])
-                latency = str_to_num(vals[2])
-                if time is not False and time != 0 and latency is not False:
-                    data[vals[0]].append((time, latency))
-                elif time is False:
-                    text[vals[0]][vals[1]] = vals[2]
-    fig, ax = plt.subplots()
-    # table
-    row_labels = text.keys()
-    col_labels = text.items()[0][1].keys()
-    table_vals = []
-    for row in row_labels:
-        table_vals.append([text[row][param] for param in col_labels])
-    table = ax.table(cellText=table_vals,
-                     rowLabels=row_labels,
-                     colLabels=col_labels,
-                     loc='upper right')
-    table.set_fontsize(8)
-    table.scale(0.85, 1.1)
-    # lines
-    for field, points in data.items():
-        x = [point[0] for point in points]
-        y = [point[1] for point in points]
-        ax.plot(x, y, LINE_TYPES[field], label=field.strip('[]').lower())
+def latency(log_files, save_or_show):
+    for log_file in log_files:
+        data = defaultdict(list)
+        with open(log_file) as f:
+            for line in f:
+                vals = [x.strip() for x in line.split(',')]
+                if len(vals) == 3 and vals[0] in FIELDS:
+                    time = str_to_num(vals[1])
+                    latency = str_to_num(vals[2])
+                    if time is not False and time != 0 and latency is not False:
+                        data[vals[0]].append((time, latency))
+        for field, points in data.items():
+            x = [point[0] for point in points]
+            y = [point[1] for point in points]
+            plt.plot(x, y, label='%s %s' %
+                     (log_file.replace('.log', ''), field.strip('[]').lower()))
+    plt.title('Latency')
     plt.xlabel('time (ms)')
     plt.ylabel('latency (us)')
-    plt.title(title).set_y(1.2)
-    legend = ax.legend(loc='right')
+    legend = plt.legend()
     for label in legend.get_texts():
-        label.set_fontsize('medium')
-    plt.title(title)
-    plt.show()
+        label.set_fontsize('small')
+    if save_or_show == 'show':
+        plt.show()
+    else:
+        plt.savefig('latency.pdf', bbox_inches=0)
+        print 'Latency graph saved to latency.pdf'
 
 
 def main():
     if len(sys.argv) < 3:
         print 'Wrong number of arguments.'
-        print 'Usage: python graph.py [throughput | latency] [log files]'
+        print 'Usage: python graph.py [throughput | latency] [save | show(default)] [log files]'
     else:
         args = sys.argv
         option = args[1]
-        log_files = args[2:]
+        save_or_show = 'show'
+        if args[2] == 'save' or args[2] == 'show':
+            save_or_show = args[2]
+            log_files = args[3:]
+        else:
+            log_files = args[2:]
         if option == 'throughput':
-            throughput(log_files)
+            throughput(log_files, save_or_show)
         elif option == 'latency':
-            latency(log_files)
+            latency(log_files, save_or_show)
 
 
 if __name__ == '__main__':
